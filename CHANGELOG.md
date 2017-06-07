eb4ab8f78e3d622a729a61bdde942eb1e9619c84
Mihael Safaric <mihael.safaric@gmail.com>
2017-06-07 18:41:05 +0200

    Loosen FormObjectOptions interface

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+### v0.2.3
+
+  * Loosened FormObjectOptions interface
--- src/interfaces/form-object-options.interface.ts
+++ src/interfaces/form-object-options.interface.ts
@@ -1,9 +1,9 @@
 // import { FormModel } from './form-model.interface';
 
 export interface FormObjectOptions {
-  attributesTransformer: Function; // (model: FormModel) => Array<string>;
-  hasManyTransformer: Function;
-  belongsToTransformer: Function;
-  getModelType: Function;
-  getConfig: Function;
+  attributesTransformer?: Function; // (model: FormModel) => Array<string>;
+  hasManyTransformer?: Function;
+  belongsToTransformer?: Function;
+  getModelType?: Function;
+  getConfig?: Function;
 }
