30a373d771acd743476952831898fc1aa195841a
Mihael Safaric <mihael.safaric@gmail.com>
2017-10-16 16:25:07 +0200

    Remove transpiled files

--- src/helpers/is-number/is-number.helper.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isNumber(value) {
-    return typeof value === 'number' && value === value;
-}
-exports.isNumber = isNumber;
