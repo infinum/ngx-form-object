1365c6c0464a9bb2c63b2912e20eb8698ec41f98
Mihael Safaric <mihael.safaric@gmail.com>
2019-07-11 22:39:04 +0200

    Remove demo project

--- demo/src/app/models/company.model.ts
+++
@@ -1,9 +0,0 @@
-import { FormModel, Attribute } from 'ngx-form-object';
-import { SimpleModel } from 'app/services/datastore/datastore.service';
-
-export class Company extends SimpleModel implements FormModel {
-  config = null;
-
-  @Attribute()
-  name: string;
-}
