2704dcad43337ea7af2fde52e8dc4899d6f1274b
Filip Voska <filip.voska@infinum.hr>
2019-10-07 10:17:10 +0200

    Fix sample app tests

--- projects/ngx-form-object-sample-app/src/app/app.component.spec.ts
+++ projects/ngx-form-object-sample-app/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxFormObjectModule } from 'ngx-form-object';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         NgxFormObjectModule.forRoot(),
+        ReactiveFormsModule,
         RouterTestingModule,
       ],
       declarations: [
