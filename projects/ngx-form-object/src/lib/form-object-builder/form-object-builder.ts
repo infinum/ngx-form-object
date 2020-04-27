c220bcfe3773d856397ffc5aa48d8dffba253911
Mihael Safaric <mihael.safaric@gmail.com>
2020-04-27 15:34:20 +0200

    Remove console.warn while creating relationship forms

--- projects/ngx-form-object/src/lib/form-object-builder/form-object-builder.ts
+++ projects/ngx-form-object/src/lib/form-object-builder/form-object-builder.ts
@@ -121,7 +121,8 @@ export class FormObjectBuilder {
       const formStore: FormStore = this.create(modelFormObject);
       return formStore;
     } else {
-      console.warn(`There is no function specified for creating form object for ${relationshipName.toString()}.`);
+      // There is no function specified for creating form object for ${relationshipName.toString()}
+      // noop
     }
   }
 }
