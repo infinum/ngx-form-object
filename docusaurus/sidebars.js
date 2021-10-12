2e72cc32c2c467ae5e816ebf5f99af11ba15ed2d
Mihael Šafarić <mihael.safaric@gmail.com>
2021-10-12 12:34:23 +0200

    Group sidebar items

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -1,5 +1,19 @@
 module.exports = {
-  mainSidebar: {
-    Docusaurus: ['getting-started', 'form-object', 'decorators', 'extended-form-controls', 'migration-guide'],
-  },
+  mainSidebar: [
+    {
+      type: 'category',
+      label: 'Getting started',
+      items: ['introduction', 'getting-started/installation', 'getting-started/basic-usage']
+    },
+    {
+      type: 'category',
+      label: 'API reference',
+      items: [
+        'form-object',
+        'decorators',
+        'extended-form-controls'
+      ]
+    },
+    'migration-guide'
+  ]
 };
