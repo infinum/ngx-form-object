b6a6bf6a20198ebdcefa8f940b7a83667ac24a84
Tomislav Habalija <tomislav.habalija@infinum.hr>
2022-01-31 15:28:36 +0100

    Documentation: homepage styling

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
-const features = [];
+const features = [
+  {
+    title: "Built for Angular",
+    description: "Built on top of Angular's reactive forms",
+    imageUrl: "./img/undraw_code_review_re_woeb.svg",
+  },
+  {
+    title: "Easy setup",
+    description: "Easily integrate to your exiting app",
+    imageUrl: "./img/undraw_preferences_re_49in.svg",
+  },
+  {
+    title: "Simple to use",
+    description: "Get started in a matter of minutes",
+    imageUrl: "./img/undraw_monitor_iqpq.svg",
+  },
+];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
+    <div className={clsx("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -25,22 +41,21 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
+      <header className={clsx("hero", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                "button button--outline button--primary button--lg",
+                styles.getStarted
               )}
-              to={useBaseUrl('docs')}>
+              to={useBaseUrl("docs")}
+            >
               Get Started
             </Link>
           </div>
