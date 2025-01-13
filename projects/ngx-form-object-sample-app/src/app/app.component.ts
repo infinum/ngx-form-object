bdd17c9428cd9c1b282d6115281c3d3a70ba12e3
Mihael Šafarić <mihael.safaric@gmail.com>
2025-01-13 14:45:30 +0100

    Angular 19 update (#92)
    * Update node requirement
    
    * Update dependencies
    
    * Update Angular 19

--- projects/ngx-form-object-sample-app/src/app/app.component.ts
+++ projects/ngx-form-object-sample-app/src/app/app.component.ts
@@ -4,9 +4,10 @@ import { UserFormObject } from './models/user.form-object';
 import { User } from './models/user.model';
 
 @Component({
-	selector: 'ngxfosa-root', // ngxfosa === ngx-form-object sample app
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss'],
+    selector: 'ngxfosa-root', // ngxfosa === ngx-form-object sample app
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
 	public userFormStore: FormStore;
