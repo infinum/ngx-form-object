1b94c4f10c25c32b7a63536edfe74cdea0b9c072
Mihael Šafarić <mihael.safaric@gmail.com>
2025-01-13 15:47:06 +0100

    Update dependencies (#93)
    * Update rxjs
    
    * Update node
    
    * Update jasmine
    
    * Update karma
    
    * Remove jsdoc
    
    * Update @typescript-eslint
    
    * Update nested dependencies
    
    * Update eslint-typescript

--- projects/ngx-form-object-sample-app/src/app/app.component.spec.ts
+++ projects/ngx-form-object-sample-app/src/app/app.component.spec.ts
@@ -1,16 +1,16 @@
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxFormObjectModule } from 'ngx-form-object';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-	beforeEach(async(() => {
+	beforeEach(() => {
 		TestBed.configureTestingModule({
 			imports: [NgxFormObjectModule.forRoot(), ReactiveFormsModule, RouterTestingModule],
 			declarations: [AppComponent],
 		}).compileComponents();
-	}));
+	});
 
 	it('should create the app', () => {
 		const fixture = TestBed.createComponent(AppComponent);
